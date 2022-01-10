24354b6e498acc8434b7c192b3f83ad11fb5a10d
Mohaiminul <mohaiminul23r@gmail.com>
2022-01-10 13:03:29 +0600

    update function

--- js/function.js
+++ js/function.js
@@ -24,7 +24,9 @@
   $("#close-sidebar").click(function() {
     $(".page-wrapper").removeClass("toggled");
   });
+
   $("#show-sidebar").click(function() {
     $(".page-wrapper").addClass("toggled");
   });
+  
 });
